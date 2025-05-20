5ff6c66b63cdb3e5bd936a895d34d725583eeb48
Duru Nef Özmen <duru.ozmen@sabanciuniv.edu>
2025-05-20 18:37:16 +0300

    without ticker and mongo it works PERFECTO

--- scripts/config/database.php
+++ scripts/config/database.php
@@ -21,21 +21,4 @@ function get_mysql_connection() {
     global $mysql_conn;
     return $mysql_conn;
 }
-
-// MongoDB Configuration
-$mongo_uri = 'mongodb://localhost:27017';
-$mongo_db = 'GymDB';
-
-try {
-    $mongo_client = new MongoDB\Client($mongo_uri);
-    $mongo_db = $mongo_client->selectDatabase($mongo_db);
-} catch (Exception $e) {
-    // Log MongoDB connection error but don't stop execution
-    error_log("MongoDB Connection failed: " . $e->getMessage());
-}
-
-function get_mongo_db() {
-    global $mongo_db;
-    return $mongo_db;
-}
 ?> 
--- scripts/user/index.php
+++ scripts/user/index.php
@@ -53,19 +53,6 @@
                 </div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title mb-0">Support Tickets</h5>
-                    </div>
-                    <div class="card-body">
-                        <a href="support.php" class="btn btn-primary">Manage Support Tickets</a>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
